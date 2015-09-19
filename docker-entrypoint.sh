ace8c0eb00402069653c747b761682dce75b073a
fentas <jan.guth@gmail.com>
2015-09-19 14:20:49 +0200

    clean up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -z ${AUTHORIZED_KEYS} ]; then
-  if [[ ${AUTHORIZED_KEYS} =~ '(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]' ]]; then
-    curl -Ls ${AUTHORIZED_KEYS} | sudo tee -a /root/.ssh/authorized_keys
+if [ ! -z "${AUTHORIZED_KEYS}" ]; then
+  if [[ "${AUTHORIZED_KEYS}" =~ '(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]' ]]; then
+    curl -Ls "${AUTHORIZED_KEYS}" | sudo tee -a /home/cattle/.ssh/authorized_keys
   else
-    echo ${AUTHORIZED_KEYS} | sudo tee -a /root/.ssh/authorized_keys
+    echo "${AUTHORIZED_KEYS}" | sudo tee -a /home/cattle/.ssh/authorized_keys
   fi
 fi
 
